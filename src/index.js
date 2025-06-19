7e159351d4e68bde802285a644198050f4530a6d
Filipe Gonsalves <4081921+jfgonsalves@users.noreply.github.com>
2025-06-19 20:17:17 +1000

    fix: server connection check import

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import App from "./App";
 import "./index.css";
 import theme from "./theme";
 import { CustomToast } from "./components/common/Toast";
+import { ServerConnectionCheck } from "./components/common/ServerConnectionCheck";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
