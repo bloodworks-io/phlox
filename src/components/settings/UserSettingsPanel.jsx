3393004918190dae14ce8ba7f40a0ffb031fa556
Filipe Gonsalves <4081921+jfgonsalves@users.noreply.github.com>
2026-08-15 19:12:47 +1000

    fix(ui): hide languages without localisation

--- src/components/settings/UserSettingsPanel.jsx
+++ src/components/settings/UserSettingsPanel.jsx
@@ -10,7 +10,7 @@ import ChatSettingsPanel from "./ChatSettingsPanel";
 import { isChatEnabled } from "../../utils/helpers/featureFlags";
 import { settingsApi } from "../../utils/api/settingsApi";
 import { syncLanguage } from "../../i18n";
-import { PREFERRED_LANGUAGE_OPTIONS, getLanguageName } from "../../utils/i18n/languages";
+import { UI_LANGUAGES, getLanguageName } from "../../utils/i18n/languages";
 
 const ADVANCED_OPTIONS_SCHEMA = [
   {
@@ -199,7 +199,7 @@ const UserSettingsPanel = ({
                         value={selectedLanguage}
                         onChange={(e) => handleLanguageChange(e.target.value)}
                         className="input-style">
-                        {PREFERRED_LANGUAGE_OPTIONS.map((lang) => (
+                        {UI_LANGUAGES.map((lang) => (
                           <option key={lang.code} value={lang.code}>
                             {lang.native} ({lang.name})
                           </option>
